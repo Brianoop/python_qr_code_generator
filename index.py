aac79e16c6e6806373231d960e89f16d8ad673c6
Brianoop <brianangoda99@gmail.com>
2022-09-28 12:07:44 +0300

    added longer list of strings

--- index.py
+++ index.py
@@ -2,18 +2,20 @@
 import qrcode
  
 # Data to be encoded
-strings = ["first", "second", "third", "fourth", "fifth"]
+strings = ["first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eigth", "nineth", "tenth"]
  
- # create an instance from qrcode
-qr = qrcode.QRCode(version = 1,
-                   box_size = 20,
-                   border = 5)
+ 
  
 for x in strings:
 	# Encoding data using make() function
 	#img = qrcode.make(x)
 	# Saving as an image file
 	#img.save('images/'+ x + '_QRCode' + '.png') 
+
+	# create an instance from qrcode
+	qr = qrcode.QRCode(version = 1,
+                   box_size = 20,
+                   border = 5)
 
 	qr.add_data(x)
 	qr.make(fit = True)
